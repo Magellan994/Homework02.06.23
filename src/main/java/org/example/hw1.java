d9858c256069de50c1456bf6d24c191574bcaea9
Magellan994 <khukha994@gmail.com>
2023-06-03 11:45:10 +0300

    Initial commit

---
+++ src/main/java/org/example/hw1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class hw1 {
+    public static void main (String[] args){
+        System.out.println(addTenPrecent(9));
+    }
+    public static double addTenPrecent (int x ){
+        double p = (x*1.1);
+        return p;
+    }
+
+}
+
+//int x
+
+// Int
---
+++ src/main/java/org/example/hw2.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+public class hw2 {
+
+    public static void div (int a, int b){
+     int t = a / b;
+
+            System.out.println(t);
+    }
+    public static void main (String[] args) {
+        int a = 6;
+        int b = 6;
+        div(a, b);
+    }
+}
+
+
+
+// Int a ,int b.
+
+//Int
